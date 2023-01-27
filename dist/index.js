9ae02afc194d7920894e607f7e89dd2b8587bdeb
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-01-27 22:06:51 +0300

    feat: socket.io connections

--- dist/index.js
+++ dist/index.js
@@ -15,22 +15,82 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+const port = process.env.PORT || 5000;
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server);
-const port = process.env.PORT || 5000;
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-app.get('/', (req, res) => {
-    res.send("Hello socket.io!");
-});
-io.on('connection', (socket) => {
-    console.log('a user connected');
+const messages = [{
+        message: "hello123",
+        id: "2314124fs",
+        user: {
+            name: "R1",
+            id: "2314124fsd",
+        }
+    },
+    {
+        message: "hello23",
+        id: "2314124fsr32",
+        user: {
+            name: "R2",
+            id: "2314124fs32r",
+        }
+    }];
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000'
+}));
+app.use(express_1.default.json());
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
 });
 const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
 });
 startApp();
+app.get('/', (req, res) => {
+    res.send("Hello socket.io!");
+});
+const users = new Map();
+io.on('connection', (socket) => {
+    users.set(socket, {
+        name: "anonym",
+        id: `${new Date().getTime()}`,
+    });
+    socket.on("client-name-sent", (name) => {
+        if (typeof name !== "string") {
+            return;
+        }
+        const user = users.get(socket);
+        user.name = name;
+    });
+    socket.on('client-message-sent', (message, successFn) => {
+        if (typeof message !== "string" || message.length > 20) {
+            successFn("Message length should be less than 20 chars");
+            return;
+        }
+        const user = users.get(socket);
+        let item = {
+            message: message,
+            id: `${new Date().getTime()}`,
+            user: {
+                name: user.name,
+                id: user.id,
+            }
+        };
+        messages.push(item);
+        io.emit('new-message-sent', item);
+        successFn(null);
+    });
+    socket.on("client-typed", () => {
+        socket.broadcast.emit('user-typing', users.get(socket));
+    });
+    socket.emit('initMessages-published', messages);
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        users.delete(socket);
+        console.log('user disconnected');
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,104 @@ import {Request, Response} from "express";
 import express from "express";
 import http from "http";
 import {Server} from "socket.io";
+import cors from "cors";
 
+const port = process.env.PORT || 5000
 
 const app = express()
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const messages: Array<any> = [{
+    message: "hello123",
+    id: "2314124fs",
+    user: {
+        name: "R1",
+        id: "2314124fsd",
+    }
 
-const port = process.env.PORT || 5000
+},
+    {
+        message: "hello23",
+        id: "2314124fsr32",
+        user: {
+            name: "R2",
+            id: "2314124fs32r",
+        }
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
+    }]
 
-app.get('/', (req, res) => {
-    res.send("Hello socket.io!")
-});
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
-io.on('connection', (socket: any) => {
-    console.log('a user connected');
+app.use(express.json());
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
 });
 
 const startApp = async () => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
 }
 
 startApp();
+
+app.get('/', (req, res) => {
+    res.send("Hello socket.io!")
+});
+
+const users = new Map();
+
+io.on('connection', (socket: any) => {
+
+    users.set(socket, {
+            name: "anonym",
+            id: `${new Date().getTime()}`,
+    })
+
+    socket.on("client-name-sent", (name: string) => {
+        if (typeof name !== "string") {
+            return
+        }
+        const user = users.get(socket)
+        user.name = name;
+    })
+
+    socket.on('client-message-sent', (message: string, successFn: any) => {
+        if (typeof message !== "string" || message.length > 20) {
+            successFn("Message length should be less than 20 chars")
+            return
+        }
+
+        const user = users.get(socket)
+
+        let item = {
+            message: message,
+            id: `${new Date().getTime()}`,
+            user: {
+                name: user.name,
+                id: user.id,
+            }
+        };
+        messages.push(item)
+        io.emit('new-message-sent', item)
+        successFn(null)
+    });
+
+    socket.on("client-typed", () => {
+        socket.broadcast.emit('user-typing', users.get(socket))
+    });
+
+    socket.emit('initMessages-published', messages);
+
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        users.delete(socket)
+        console.log('user disconnected');
+    });
+});
