5ec6660c35d515118c613c9a66a21ef17f5a0c68
dmitrykrtv <dmitry.korotayev@gmail.com>
2023-01-27 15:16:23 +0300

    initialize deploying to vercel

--- dist/index.js
+++ dist/index.js
@@ -8,13 +8,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
 const port = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    let helloMessage = "hello incubator!!";
-    res.send(helloMessage);
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+app.get('/', (req, res) => {
+    res.send("Hello socket.io!");
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
     app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import {Request, Response} from "express";
-import express = require("express");
+import express from "express";
+import http from "http";
+import {Server} from "socket.io";
+
 
 const app = express()
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
 const port = process.env.PORT || 5000
 
-app.get("/", (req: Request, res: Response) => {
-    let helloMessage = "hello incubator!!";
-    res.send(helloMessage)
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+app.get('/', (req, res) => {
+    res.send("Hello socket.io!")
+});
+
+io.on('connection', (socket: any) => {
+    console.log('a user connected');
+});
 
 const startApp = async () => {
     app.listen(port, () => {
